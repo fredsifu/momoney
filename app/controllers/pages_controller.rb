4460fb12be14f56727212aee4be111a2bc017eb7
Frederic Scheubel <fredsifu@hotmail.com>
2018-08-30 16:25:22 -0400

    Loan types intro pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,15 @@ class PagesController < ApplicationController
   def index
   end
 
+  def personal
+  end
+
+  def car
+  end
+
+  def home
+  end
+
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     root to: 'pages#index'
 
     get '/about', to: 'pages#about'
+    get '/personal', to: 'pages#personal'
+    get '/car', to: 'pages#car'
+    get '/home', to: 'pages#home'
 
     resources :user_profiles
     resources :addresses
