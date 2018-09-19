ec234eb8a9734e8dcbab90133f62cac16b8f2b69
Frederic Scheubel <fredsifu@hotmail.com>
2018-09-19 17:58:05 -0400

    Adding employment questions to personal loans

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,3 +1,16 @@
 class Employment < ApplicationRecord
   belongs_to :user
+
+  enum status: %i[full_time_employee part_time_employee self_employed seasonal_employee unemployed]
+
+  def self.statuses_for_select
+    statuses.map { |status, _|
+      [I18n.t("activerecord.attributes.#{model_name.i18n_key}.statuses.#{status}"),
+       Employment.statuses[status]]
+    }.to_h
+  end
+
+  def selected_status
+    Employment.statuses[status]
+  end
 end
